059826ed24618f724482d949f5238000b82d9bbf
home <home@WINDOWS-PQ21EPG>
2021-12-31 10:12:38 +0530

    Add project files.

---
+++ WebApplication8/Student.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8
+{
+    public class Student : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult student()
+        {
+            return View();
+        }
+        public IActionResult studentID()
+        {
+            return View();
+        }
+        public IActionResult studentName()
+        {
+            return View();
+        }
+    }
+}
